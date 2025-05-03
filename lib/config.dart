7989a32b8ebf2af3831bb92b66730a4eab0d7d60
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2025-05-03 11:37:23 -0400

    changes

---
+++ lib/config.dart
@@ -0,0 +1,3 @@
+class Config{
+  static const brocolli_icon_url = 'https://firebasestorage.googleapis.com/v0/b/brocolli-235e5.firebasestorage.app/o/assets%2Fsteven-universe-girl.png?alt=media&token=4768af27-c344-4430-b7bd-27d9bac7fc5b';
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:multi_split_view/multi_split_view.dart';
 import 'package:myapp/widgets/code_editor.dart';
 import 'package:myapp/widgets/text_area.dart';
 
-class HomePage extends StatelessWidget {
+import '../widgets/chat.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final MultiSplitViewController _controller = MultiSplitViewController(areas: [
+    Area(builder: (context, area) => Draft.yellow()),
+    Area(builder: (context, area) => ChatPage())
+  ]);
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(flex: 1, child: TextArea()),
-                VerticalDivider(width: 1),
-                Expanded(flex: 1, child: CodeEditor()),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+    return MultiSplitView(controller: _controller);
   }
 }
